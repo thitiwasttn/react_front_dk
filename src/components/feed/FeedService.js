ed893a8c03deb0dc287b32624bb1ec524cd4036c
GA007007\Admin <thitiwas.n@g-able.com>
2021-11-23 11:56:34 +0700

    add env

--- src/components/feed/FeedService.js
+++ src/components/feed/FeedService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getFeed = () => {
-    return axios.get(`http://61.19.242.56:1337/posts/findFeed`)
+    return axios.get(`${process.env.REACT_APP_BACKEND_URL}posts/findFeed`)
         .then(res => {
             console.log('res.data', res.data);
         })
