d6be5a9b60c232fd8e108a54b96fb64e338a621b
GA007007\Admin <thitiwas.n@g-able.com>
2021-11-24 10:09:29 +0700

    up

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import reducer from './reducer';
-import {createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 import {persistStore, persistReducer} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import thunk from 'redux-thunk'
+
 
 const persistConfig = {
     key: 'root',
@@ -9,9 +11,24 @@ const persistConfig = {
 }
 
 const persistedReducer = persistReducer(persistConfig, reducer)
+const middlewares = [thunk]
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    // other store enhancers if any
+);
+
 
 export default () => {
-    let store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    let store = createStore(persistedReducer, enhancer)
     let persistor = persistStore(store)
     return {store, persistor}
 }
