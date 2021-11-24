b0744fd9661b2ec75c6be886e1fe904f00e9be2b
GA007007\Admin <thitiwas.n@g-able.com>
2021-11-24 10:03:15 +0700

    up

--- src/components/postInput/PostInput.js
+++ src/components/postInput/PostInput.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
+import './PostInput.css'
 
 const PostInput = (props) => {
+
+    const [dataState, setDataState] = useState({
+        title: '',
+        detail: ''
+    });
+    function onInputChange(event) {
+        let tempState = {...dataState}
+        tempState[event.target.name] = event.target.value;
+        setDataState(tempState);
+    }
+
     return (
-        <div>
-            <h1>post input</h1>
+        <div className={"shadow p-3 mb-5 bg-white rounded margin-top"}>
+            <form>
+                <div className={"form-group"}>
+                    <label htmlFor="title">title</label>
+                    <input
+                        type="text"
+                        className={"form-control"}
+                        id={"title"}
+                        name={"title"}
+                        onChange={onInputChange}
+                    />
+                </div>
+                <div className={"form-group"}>
+                    <label htmlFor="title">message</label>
+                    <textarea
+                        className={"form-control"}
+                        id={"message"}
+                        name={"message"}
+                        rows="3"
+                        onChange={onInputChange}
+                    />
+                </div>
+                <div className={"margin-top text-center"}>
+                    <button className={"btn btn-outline-success"}>
+                        Post
+                    </button>
+                </div>
+            </form>
         </div>
     );
 }
--- src/store.js
+++ src/store.js
@@ -11,7 +11,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, reducer)
 
 export default () => {
-    let store = createStore(persistedReducer)
+    let store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
     let persistor = persistStore(store)
     return {store, persistor}
 }
